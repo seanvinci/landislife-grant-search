85789860267c29546caca7fe9fa2b4bf6b5ec05f
“Sean <seanvinci@gmail.com>
2017-08-21 09:52:17 -0400

    Continue styling and functionality of primary nav.

--- js/core.js
+++ js/core.js
@@ -3,6 +3,15 @@
 var $window = $(window);
 var $body = $('body');
 var $navMain = $('.nav-main');
+var $pageSubnav = $('.page-subnav');
+var $purchaseDetail = $('.purchase-detail');
+var $eventDetail = $('.event-detail');
+var $courseSubnav = $('.course-subnav');
+var $modal = $('.modal-overlay');
+var viewport_width = $window.outerWidth(true);
+var $hasSubnav = $('.has-subnav');
+var $hasSubnavAnchor = $('> a', $hasSubnav);
+var $hasSubnavUl = $hasSubnavAnchor.next('ul');
 
 
 // FUNCTIONS
@@ -17,6 +26,28 @@ function toggleNav() {
     $hasSubnavUl.css('display', '');
   } else {
     $body.addClass('nav-open clip');
+  }
+}
+
+function resetNav() {
+  $body.removeClass('nav-open clip');
+  $hasSubnav.removeClass('nav-active');
+  $hasSubnavUl.css('display', '');
+}
+
+// Check if user has scrolled more than 200px
+// If so add class of 'nav-small' to body tag
+function smallNav() {
+  var scroll_top = $window.scrollTop();
+  var viewport_width = $window.outerWidth(true);
+  if (viewport_width > 768) {
+    if (scroll_top > 130) {
+      $body.addClass('nav-small');
+    } else {
+      $body.removeClass('nav-small');
+    }
+  } else {
+    $body.removeClass('nav-small');
   }
 }
 
@@ -40,6 +71,15 @@ $(document).ready(function() {
   // Close navigation when anywhere in the webpage is clicked...
   $('.nav-overlay').click(function() {
     resetNav();
+  });
+
+  // show and hide sub navigation on mobile and tablet
+  $('.nav-main nav .has-subnav > a').click(function(e) {
+    if (viewport_width < 1024) {
+      e.preventDefault();
+      $(this).closest('.has-subnav').toggleClass('nav-active');
+      $(this).next('ul').slideToggle(200);
+    }
   });
 
 });
